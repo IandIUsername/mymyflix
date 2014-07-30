0dfb2b7076b7cf9ebd11cd2d048e8228310974e7
Scott <z89quark@gmail.com>
2014-07-30 00:28:32 +0000

    reformatted the index page with %br

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -6,12 +6,7 @@ class VideosController < ApplicationController
     @categories = Category.all
     @search = Video.search_by_title(params[:search_by_title])
     @videos = Video.all
-    comedy = Category.first
-    drama = Category.second
-    @comedies = Video.where(category_id: 1)
-    @drama    = Video.where(category_id: 2)
-    @comedies = comedy.videos
-    @dramas = drama.videos   
+    
   end
   
   def show
