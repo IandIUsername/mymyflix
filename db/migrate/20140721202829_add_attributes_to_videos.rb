a1c1e54248bd8db4548c23b9f1d1ebddb603d434
Scott <z89quark@gmail.com>
2014-10-30 17:14:54 +0000

    datatype database error fixed for heroku to function

--- db/migrate/20140721202829_add_attributes_to_videos.rb
+++ db/migrate/20140721202829_add_attributes_to_videos.rb
@@ -1,6 +1,6 @@
 class AddAttributesToVideos < ActiveRecord::Migration
   def change
-    add_column :videos, :description, :text_area
+    add_column :videos, :description, :text
     add_column :videos, :small_cover_url, :string
     add_column :videos, :large_cover_url, :string
     
