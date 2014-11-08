ec8aa6bb151c481637fb4c6690c40d04455f2def
Scott <z89quark@gmail.com>
2014-11-08 05:03:42 +0000

    minor change to redis.rb removed localhost

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,5 @@
 $redis = Redis.new(:host => 'localhost', :port => 6379)
 
-uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379")
+uri = URI.parse(ENV["REDISTOGO_URL"] )
 #REDIS = Redis.new(:url => ENV['REDISTOGO_URL'])
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
