a3492cacca2213cc1041890144992a6dea7b8d8e
Scott <z89quark@gmail.com>
2014-11-08 04:14:00 +0000

    smtp in production syntax issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,17 +19,19 @@ Myflix::Application.configure do
   config.active_support.deprecation = :notify
   
   config.action_mailer.delivery_method = :smtp
+  
   config.action_mailer.smtp_settings = { 
     :port    => ENV['MAILGUN_SMTP_PORT'],
     :address  => ENV['MAILGUN_SMTP_SERVER'],
     :user_name => ENV['MAILGUN_SMTP_LOGIN'],
     :password  => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain    => www.boiling-hollows-3169.herokuapp.com
+    :domain    => 'www.boiling-hollows-3169.herokuapp.com',
     :authentication => 'plain',
-    :enable_starttls_auto => true }
+     }
     
+  ActionMailer::Base.delivery_method = :smtp
   
-   ENV["REDISTOGO_URL"] = 'redis://redistogo:30346774d912f6130a5d981b90eb01bd@greeneye.redistogo.com:11024/
+   ENV["REDISTOGO_URL"] = 'redis://redistogo:30346774d912f6130a5d981b90eb01bd@greeneye.redistogo.com:11024/'
 
- # ENV["REDISTOGO_URL"] = 'redis://username:password@my.host:6389'
+ 
 end
