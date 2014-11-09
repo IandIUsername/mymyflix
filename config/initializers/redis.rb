d10b78740fbe4db0b47cdb41897981e333d38d31
Scott <z89quark@gmail.com>
2014-11-09 00:36:13 +0000

    replace redis_provider with redistogo_url

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,8 @@
 $redis = Redis.new(:host => 'localhost', :port => 6379)
+# #uri = URI.parse(URI.encode(ENV["REDIS_PROVIDER"].strip))
+# uri = URI.parse(ENV["REDIS_PROVIDER"] )
+# #REDIS = Redis.new(:url => ENV['REDISTOGO_URL'])
+# REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
-uri = URI.parse(ENV["REDIS_PROVIDER"] )
-#REDIS = Redis.new(:url => ENV['REDISTOGO_URL'])
+uri = URI.parse(ENV["REDISTOGO_URL"])
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
